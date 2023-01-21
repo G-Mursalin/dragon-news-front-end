53756baf0f2ba9260da885059ee04bdcd2b48202
Golam Mursalin <golammursalin309@gmail.com>
2023-01-21 17:23:01 +0600

    left side bar and right side bar added

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-  return <div>LeftSideNav</div>;
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div>
+      <h4>All Catagories: {categories.length}</h4>
+      <div>
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftSideNav;
--- src/Pages/Shared/RightSideNav.js/RightSideNav.js
+++ src/Pages/Shared/RightSideNav.js/RightSideNav.js
@@ -1,7 +1,49 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
+import Carousel from "react-bootstrap/Carousel";
+
+import Brand1 from "./../../../assets/brands/Brand1.png";
+import Brand2 from "./../../../assets/brands/Brand2.png";
 
 const RightSideNav = () => {
-  return <div>RightSideNav</div>;
+  return (
+    <div>
+      <div className="d-grid gap-2">
+        <Button variant="outline-primary">
+          <FaGoogle /> Login with Google
+        </Button>
+        <Button variant="outline-dark">
+          <FaGithub /> Login with GitHub
+        </Button>
+      </div>
+
+      <div>
+        <h5 className="mt-4">Find Us On</h5>
+        <ListGroup className="gap-3">
+          <ListGroup.Item>Facebook</ListGroup.Item>
+          <ListGroup.Item>Youtube</ListGroup.Item>
+          <ListGroup.Item>Twitter</ListGroup.Item>
+          <ListGroup.Item>WhatsApp</ListGroup.Item>
+          <ListGroup.Item>Discort</ListGroup.Item>
+        </ListGroup>
+      </div>
+
+      <div>
+        <Carousel>
+          <Carousel.Item interval={500}>
+            <img className="d-block w-100" src={Brand1} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <img className="d-block w-100" src={Brand2} alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default RightSideNav;

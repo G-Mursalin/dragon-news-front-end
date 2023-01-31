bf3249f3569bc3068fab6929d55b667754bb5297
Golam Mursalin <golammursalin309@gmail.com>
2023-02-01 00:06:45 +0600

    gitHub login added

--- src/Pages/Shared/RightSideNav.js/Logins.js
+++ src/Pages/Shared/RightSideNav.js/Logins.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
 import { FaGithub, FaGoogle } from "react-icons/fa";
@@ -8,10 +8,25 @@ import { AuthContext } from "../../../Contexts/AuthProvider/AuthProvider";
 
 const Logins = () => {
   const googleProvider = new GoogleAuthProvider();
-  const { providerLogin } = useContext(AuthContext);
+  const gitHubProvider = new GithubAuthProvider();
+  const { providerLogin, setUser } = useContext(AuthContext);
   const location = useLocation();
   const navigate = useNavigate();
   const from = location.state?.from?.pathname || "/";
+
+  const handleGitHubLogin = () => {
+    // Firebase GitHub Signup
+    providerLogin(gitHubProvider)
+      .then((result) => {
+        navigate(from, { replace: true });
+        setUser(result.user);
+        toast.success("Successfully Login");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
   const handleGoogleLogin = () => {
     // Firebase Google Signup
     providerLogin(googleProvider)
@@ -34,6 +49,7 @@ const Logins = () => {
         <FaGoogle /> Login with Google
       </Button>
       <Button
+        onClick={handleGitHubLogin}
         variant="outline-dark"
         className="d-flex align-items-center justify-content-center gap-2"
       >
